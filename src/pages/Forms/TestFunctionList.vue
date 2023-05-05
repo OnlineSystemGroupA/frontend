1f8e7b9b97a4c41e0b226c0418bf569efed3b4d8
gbccccc <78393470+gbccccc@users.noreply.github.com>
2023-05-05 11:06:21 +0800

    fix: 修复了软件功能列表软件名和软件版本无法编辑的问题

--- src/pages/Forms/TestFunctionList.vue
+++ src/pages/Forms/TestFunctionList.vue
@@ -3,10 +3,10 @@
         <h1>功能表格</h1>
         <el-form :model="form" ref="form" label-width="100px" class="demo-dynamic" :disabled = "disable">
             <el-form-item label="软件名称">
-                <el-input></el-input>
+                <el-input v-model="form.softwareName"></el-input>
             </el-form-item>
             <el-form-item label="版本号">
-                <el-input></el-input>
+                <el-input v-model="form.softwareVersion"></el-input>
             </el-form-item>
             <el-form-item v-for="(func, index) in form.functions" :key="func.key">
                 <div class="func">
