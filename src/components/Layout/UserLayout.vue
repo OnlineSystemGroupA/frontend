b06cbc5a3d7b7d3edac04c4e2e74a1b79cd486a7
gbccccc <78393470+gbccccc@users.noreply.github.com>
2023-05-26 17:19:36 +0800

    refactor: 用户主界面布局组件化

---
+++ src/components/Layout/UserLayout.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="layout">
+        <el-container>
+            <el-header>Header</el-header>
+            <el-container>
+                <el-container>
+                    <el-aside style="width: auto;">
+                        <slot name="menu"></slot>
+                    </el-aside>
+                    <el-main>
+                        <div class="main">
+                            <router-view>
+                            </router-view>
+                        </div>
+                    </el-main>
+                </el-container>
+                <el-footer style="height: auto;">
+                    <PageFooter></PageFooter>
+                </el-footer>
+            </el-container>
+        </el-container>
+    </div>
+</template>
+
+<script>
+import PageFooter from '../../components/Footers/PageFooter.vue'
+
+export default {
+    name: 'UserLayout',
+    components: {
+        PageFooter
+    }
+}
+</script>
+
+<style scoped>
+.layout {
+    width: 100%;
+    height: 100%;
+    padding: 5%;
+}
+
+.main {
+    width: auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+}
+</style>
--- src/pages/Pages/EmployeePage.vue
+++ src/pages/Pages/EmployeePage.vue
@@ -1,52 +1,23 @@
 <template>
-    <div class="employee">
-        <el-container>
-            <el-header fixed>Header</el-header>
-            <el-container>
-                <el-container>
-                    <el-aside style="width: auto;">
-                        <EmployeeMenu>
-                        </EmployeeMenu>
-                    </el-aside>
-                    <el-main>
-                        <div class="main">
-                            <router-view>
-                            </router-view>
-                        </div>
-                    </el-main>
-                </el-container>
-                <el-footer style="height: auto;">
-                    <PageFooter></PageFooter>
-                </el-footer>
-            </el-container>
-        </el-container>
-    </div>
+    <UserLayout>
+        <template #menu>
+            <EmployeeMenu/>
+        </template>
+    </UserLayout>
 </template>
 
 <script>
-import EmployeeMenu from '../../components/Menus/EmployeeMenu.vue';
-import PageFooter from '../../components/Footers/PageFooter.vue';
+import UserLayout from "@/components/Layout/UserLayout.vue";
+import EmployeeMenu from "@/components/Menus/EmployeeMenu.vue";
+
 export default {
     name: 'EmployeePage',
     components: {
-        EmployeeMenu,
-        PageFooter
+        UserLayout,
+        EmployeeMenu
     }
 }
 </script>
 
 <style scoped>
-.employee {
-    width: 100%;
-    height:100%;
-    padding: 50px;
-}
-
-.main {
-    width: auto;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-}
 </style>
