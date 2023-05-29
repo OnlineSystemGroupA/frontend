6dc66cdb2ff1fbc297c5b199a217139387498acf
gbccccc <78393470+gbccccc@users.noreply.github.com>
2023-05-29 14:16:25 +0800

    feat: 添加用户界面面包屑

---
+++ src/components/Headers/UserHeader.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+            <el-breadcrumb-item
+                v-for="bread in breads"
+                :key="bread.title"
+                :to="bread.to"
+                class="breadcrumb">
+                {{ bread.title }}
+            </el-breadcrumb-item>
+        </el-breadcrumb>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            breads: []
+        }
+    },
+
+    methods: {
+        updateBreads() {
+            this.breads = this.$route.matched.map((v) => {
+                return {to: v.path, title: v.meta.title}
+            });
+        }
+    },
+
+    watch: {
+        "$route.path"() {
+            this.updateBreads();
+        },
+    },
+
+    created() {
+        this.updateBreads();
+    }
+}
+</script>
+
+<style scoped lang="less">
+.breadcrumb {
+    /deep/ .el-breadcrumb__inner {
+        color: #FFFFFF;
+    }
+
+    /deep/ .el-breadcrumb__inner:hover {
+        color: #FFFFFF;
+    }
+}
+</style>
--- src/components/Layout/UserLayout.vue
+++ src/components/Layout/UserLayout.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="layout">
         <el-container>
-            <el-header class="header">Header</el-header>
+            <el-header class="user-header">
+                <UserHeader/>
+            </el-header>
             <el-container>
                 <el-aside width="13%" class="aside">
                     <slot name="menu"></slot>
@@ -21,9 +23,12 @@
 
 <script>
 
+import UserHeader from "@/components/Headers/UserHeader.vue";
+
 export default {
     name: 'UserLayout',
     components: {
+        UserHeader
     }
 }
 </script>
@@ -32,6 +37,14 @@ export default {
 .layout {
     width: 100%;
     height: 100vh;
+}
+
+.user-header {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    background-color: #6A005F;
+    border-bottom: 2px solid #895984;
 }
 
 .main-container {
