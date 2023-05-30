07d58aa923c31e98780d674ffe8dba47c59df0b9
gbccccc <78393470+gbccccc@users.noreply.github.com>
2023-05-30 13:33:50 +0800

    feat: 用户界面侧栏添加边缘阴影

--- src/components/Layout/UserLayout.vue
+++ src/components/Layout/UserLayout.vue
@@ -33,7 +33,7 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="less">
 .layout {
     width: 100%;
     height: 100vh;
@@ -65,5 +65,11 @@ export default {
 .aside {
     height: calc(100vh - 60px);
     overflow-y: auto;
+    z-index: 1;
+    box-shadow: 0 0 5px 0 #000000;
+
+    /deep/ .el-menu{
+        border-right: 0;
+    }
 }
 </style>
