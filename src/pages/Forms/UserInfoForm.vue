23e5940b617d30afc891c3b3394d7a6011dbdb4a
James <zhongyyoo0@163.com>
2023-06-25 22:09:39 +0800

    perf:优化了个人信息界面

--- src/pages/Forms/UserInfoForm.vue
+++ src/pages/Forms/UserInfoForm.vue
@@ -1,28 +1,87 @@
 <template>
     <div class="userInfo">
         <el-form :label-position="labelPosition" ref="loginForm" label-width="80px" :model="userInfo" :rules="rules">
-            <el-form-item label="真实姓名" prop="realName">
-                <el-input v-model="userInfo.realName" placeholder="请输入真实姓名" clearable></el-input>
-            </el-form-item>
-            <el-form-item label="电子邮件" prop="email">
-                <el-input v-model="userInfo.email" placeholder="请输入电子邮箱" clearable=""></el-input>
-            </el-form-item>
-            <el-form-item label="联系电话" prop="telephone">
-                <el-input v-model="userInfo.telephone" placeholder="请输入联系电话" clearable=""></el-input>
-            </el-form-item>
-            <el-form-item label="公司" prop="company">
-                <el-input v-model="userInfo.company" placeholder="请输入公司" clearable=""></el-input>
-            </el-form-item>
-            <el-form-item label="公司地址" prop="address">
-                <el-input v-model="userInfo.address" placeholder="请输入公司地址" clearable=""></el-input>
-            </el-form-item>
-            
-            <el-form-item label="性别">
-                <el-radio-group v-model="userInfo.gender">
-                    <el-radio label = "男"></el-radio>
-                    <el-radio label = "女"></el-radio>
-                </el-radio-group>
-            </el-form-item>
+            <el-row>
+                <el-col style="width:50%">
+                    <el-form-item label="真实姓名" prop="realName">
+                        <el-input v-model="userInfo.realName" placeholder="请输入真实姓名" clearable></el-input>
+                    </el-form-item>
+                </el-col>
+                <el-col style="width:50%">
+                    <el-form-item label="电子邮件" prop="email">
+                        <el-input v-model="userInfo.email" placeholder="请输入电子邮箱" clearable=""></el-input>
+                    </el-form-item>
+                </el-col>
+            </el-row>
+
+            <el-row>
+                <el-col style="width:50%">
+                    <el-form-item label="联系电话" prop="telephone">
+                        <el-input v-model="userInfo.telephone" placeholder="请输入联系电话" clearable=""></el-input>
+                    </el-form-item>
+                </el-col>
+                <el-col style="width:50%">
+                    <el-form-item label="性别">
+                        <el-radio-group v-model="userInfo.gender">
+                            <el-radio label="男"></el-radio>
+                            <el-radio label="女"></el-radio>
+                        </el-radio-group>
+                    </el-form-item>
+                </el-col>
+            </el-row>
+
+            <el-row>
+                <el-col style="width:50%">
+                    <el-form-item label="公司" prop="company.name">
+                        <el-input v-model="userInfo.company.name" placeholder="请输入公司" clearable=""></el-input>
+                    </el-form-item>
+                </el-col>
+                <el-col style="width:50%">
+                    <el-form-item label="公司电话" prop="company.telephone">
+                        <el-input v-model="userInfo.company.telephone" placeholder="请输入公司电话" clearable=""></el-input>
+                    </el-form-item>
+                </el-col>
+            </el-row>
+
+            <el-row>
+                <el-col style="width:50%">
+                    <el-form-item label="公司传真" prop="company.fax">
+                        <el-input v-model="userInfo.company.fax" placeholder="请输入公司传真" clearable=""></el-input>
+                    </el-form-item>
+                </el-col>
+                <el-col style="width:50%">
+                    <el-form-item label="公司地址" prop="company.address">
+                        <el-input v-model="userInfo.company.address" placeholder="请输入公司地址" clearable=""></el-input>
+                    </el-form-item>
+                </el-col>
+            </el-row>
+
+            <el-row>
+                <el-col style="width:50%">
+                    <el-form-item label="公司邮编" prop="company.postcode">
+                        <el-input v-model="userInfo.company.postcode" placeholder="请输入公司邮编" clearable=""></el-input>
+                    </el-form-item>
+                </el-col>
+                <el-col style="width:50%">
+                    <el-form-item label="公司网址" prop="company.website">
+                        <el-input v-model="userInfo.company.website" placeholder="请输入公司网址" clearable=""></el-input>
+                    </el-form-item>
+                </el-col>
+            </el-row>
+
+            <el-row>
+                <el-col style="width:50%">
+                    <el-form-item label="公司邮箱" prop="company.e_mail">
+                        <el-input v-model="userInfo.company.e_mail" placeholder="请输入公司邮箱" clearable=""></el-input>
+                    </el-form-item>
+                </el-col>
+                <el-col style="width:50%">
+                    <el-form-item label="公司手机" prop="company.phone">
+                        <el-input v-model="userInfo.company.phone" placeholder="请输入公司手机" clearable=""></el-input>
+                    </el-form-item>
+                </el-col>
+            </el-row>
+
             <el-form-item>
                 <el-button type="primary" @click="submit">修改</el-button>
                 <el-button type="primary" @click="cancel">取消</el-button>
@@ -39,20 +98,31 @@ export default {
             userInfo: {
                 username: '杰伦',
                 realName: '周杰伦',
-                email: 'jielun@qq.com',
+                e_mail: 'jielun@qq.com',
                 telephone: '123456789',
-                company: '杰威尔公司',
-                address: '台湾省高雄市xx街道xx号',
-                gender:'男'
+                address: '台北xx街道xx号',
+                date: '2023-06-12',
+                gender: '男',
+                company: {
+                    name: '杰威尔公司',
+                    telephone: '1000000',
+                    fax: '1000000',
+                    contact: '方文山',
+                    postcode: '324330',
+                    phone: '100002',
+                    e_mail: '103948932@11.com',
+                    website: 'www.jieweier.com',
+                    address: '台湾省高雄市xx街道xx号',
+                },
             }
         }
     },
-    methods:{
+    methods: {
 
         submit() {
             alert('修改成功')
             this.$router.replace({
-                name:'clientDetail'
+                name: 'clientDetail'
             })
         },
         cancel() {
@@ -67,7 +137,7 @@ export default {
 
 <style scoped>
 .userInfo {
-    width: 40%;
+    width: 90%;
     align-items: center;
     border-radius: 30px;
     margin: 30px;
