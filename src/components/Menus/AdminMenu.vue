ac5fdddce6b9b0a0593c0dd49ec38aff69c78af8
gbccccc <78393470+gbccccc@users.noreply.github.com>
2023-06-27 14:25:31 +0800

    style: 去除部分无用代码

--- src/components/Menus/AdminMenu.vue
+++ src/components/Menus/AdminMenu.vue
@@ -1,11 +1,5 @@
 <template>
     <div style="height: 100%;">
-        <!--
-            <el-radio-group v-model="isCollapse" style="margin-bottom: 20px;">
-                <el-radio-button :label="false">展开</el-radio-button>
-                <el-radio-button :label="true">收起</el-radio-button>
-            </el-radio-group>
-        -->
         <el-menu default-active="1-4-1"
                  class="el-menu-vertical-demo"
                  @open="handleOpen"
