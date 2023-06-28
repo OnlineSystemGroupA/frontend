2b235afbdcc8dabfbb33e2e032823e9d75cf2468
gbccccc <78393470+gbccccc@users.noreply.github.com>
2023-06-28 13:58:44 +0800

    fix: 修复disabled状态下，el-tag仍可被删除的问题

--- src/components/ChooseAndSelect/SelectAndCreateTags.vue
+++ src/components/ChooseAndSelect/SelectAndCreateTags.vue
@@ -3,7 +3,7 @@
         <el-tag
             :key="tag"
             v-for="tag in dynamicTags"
-            closable
+            :closable="!disabled"
             :disable-transitions="false"
             @close="handleClose(tag)">
             {{ tag.length > 50 ? tag.substring(0, 49) + '...' : tag }}
@@ -45,7 +45,7 @@ export default {
         prop: 'options',
         event: 'change'
     },
-    props: ["options", "defaultOptions", "optionDescription"],
+    props: ["options", "defaultOptions", "optionDescription", "disabled"],
 
     data() {
         return {
